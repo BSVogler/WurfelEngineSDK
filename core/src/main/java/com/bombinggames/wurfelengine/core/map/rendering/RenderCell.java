3cd530200f280982e0720bb0ef126b53a2407501
BSVogler <e-mail@benediktvogler.de>
2017-05-01 21:07:28 +0200

    outdated javadoc

--- core/src/main/java/com/bombinggames/wurfelengine/core/map/rendering/RenderCell.java
+++ core/src/main/java/com/bombinggames/wurfelengine/core/map/rendering/RenderCell.java
@@ -131,8 +131,6 @@ public class RenderCell extends AbstractGameObject {
 	/**
 	 * Pixels per game spaces meter (edge length).<br>
 	 * 1 game meter ^= 1 GAME_EDGELENGTH<br>
-	 * The value is calculated by VIEW_HEIGHT*sqrt(2) because of the axis
-	 * shortening.
 	 */
 	public transient static final int GAME_EDGELENGTH = (int) (GAME_DIAGLENGTH / 1.41421356237309504880168872420969807856967187537694807317667973799f);
 
