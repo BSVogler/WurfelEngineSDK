0f8b8a93af6229b3f74297539683e7fbdf4c2fe7
BSVogler <e-mail@benediktvogler.de>
2017-08-09 16:35:36 +0200

    disable depth mask if not used

--- core/src/main/java/com/bombinggames/wurfelengine/core/GameView.java
+++ core/src/main/java/com/bombinggames/wurfelengine/core/GameView.java
@@ -363,6 +363,7 @@ public class GameView implements GameManager {
 				Gdx.gl.glDepthFunc(GL20.GL_LEQUAL);
 				Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 			} else {
+				Gdx.gl20.glDepthMask(false);
 				Gdx.gl20.glDisable(GL20.GL_DEPTH_TEST);
 				Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 			}
